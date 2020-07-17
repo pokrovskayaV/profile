1ccc0ecac0156c859f4ad38cbfc25ed310c8f1a1
pokrovskayaV <67792638+pokrovskayaV@users.noreply.github.com>
2020-07-17 20:53:44 +0300

    Last commit

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,19 @@ const toggleBtn = document.querySelector(".header__button");
 toggleBtn.addEventListener("click", function() {
   if (document.documentElement.hasAttribute("theme")) {
     document.documentElement.removeAttribute("theme");
-    toggleBtn.textContent=('Темная тема');
+    toggleBtn.textContent = "Темная тема";
   } else {
     document.documentElement.setAttribute("theme", "dark");
-    toggleBtn.textContent=('Светлая тема');
+    toggleBtn.textContent = "Светлая тема";
   }
 });
+
+const menu = document.querySelector(".header__item");
+const subitems = document.querySelectorAll(".header__subitems");
+
+if (document.body.clientWidth < 424) {
+  menu.addEventListener("click", function() {
+    console.log("click");
+    subitems.forEach(item => item.classList.toggle("_active"));
+  });
+}
